e098ef0f942fd28b4610ab5ee2ad111a986f7375
Ryan Seddon <seddon.ryan@gmail.com>
2013-05-27 10:40:44 +1000

    Alter how slide translate is done, using white-space nowrap has unfortunate side effects

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,12 +54,26 @@ module.exports = function(grunt) {
           'dist/<%= pkg.name %>.css': ['dist/<%= pkg.name %>.css']
         }
       }
+    },
+    sourcemappragma: {
+      dist: {
+        src: 'dist/<%= pkg.name %>.css'
+      }
     }
   });
 
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
+  grunt.registerMultiTask('sourcemappragma', 'Add source map comment pragma back in', function() {
+    this.files.forEach(function(file) {
+      var output = grunt.file.read(file.src);
+
+      output = grunt.template.process('<%= output %>\n/*@ sourceMappingURL=<%= filename %>.map */', { data: { output: output, filename: file.src[0].split('/')[1] } });
+      grunt.file.write(file.src[0], output);
+    });
+  });
+
   // Default task.
-  grunt.registerTask('default', ['sass', 'autoprefixer', 'csslint', 'cssmin']);
+  grunt.registerTask('default', ['sass', 'autoprefixer', 'csslint', 'sourcemappragma', 'cssmin']);
 
 };
